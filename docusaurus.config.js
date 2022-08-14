13ee1b964b18be9463150075bee919a194fbe62c
merchfck <logistikman@bk.ru>
2022-08-15 02:30:10 +0300

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,65 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'BasedgeBot',
+  url: 'https://vk.com/basedgebot',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'merchfck', // Usually your GitHub org/user name.
+  projectName: 'Basedge-Docs', // Usually your repo name.
+
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js')
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Basedge',
+        items: [
+          {
+            href: 'https://www.twitch.tv/basedgebot',
+            label: 'Twitch',
+            position: 'right',
+          },
+          {
+            href: 'https:/vk.com/basedgebot',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
