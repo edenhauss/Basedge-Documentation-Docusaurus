7f1ecbb016b840068239bc9ba7f6d17e71b9ed00
merchfck <logistikman@bk.ru>
2022-08-26 23:51:13 +0300

    random button main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,18 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
+
 function HomepageHeader() {
     const {siteConfig} = useDocusaurusContext();
+    let games = [
+        {link: "casino", title: "Казино"},
+        {link: "roulette", title: "Рулетка"},
+        {link: "slots", title: "Слоты"},
+        {link: "trade", title: "Трейд"},
+    ];
+
+    let num = Math.floor(Math.random() * games.length);
+
     return (
         <header className={clsx('hero hero--primary', styles.heroBanner)}>
             <div className="container">
@@ -17,8 +27,8 @@ function HomepageHeader() {
                 <div className={styles.buttons}>
                     <Link
                         className="button button--secondary button--lg"
-                        to="/docs/games/casino">
-                        Игры
+                        to={`/docs/games/${games[num]['link']}`}>
+                        {games[num]['title']}
                     </Link>
                     <Link
                         className="button button--primary button--lg"
@@ -43,3 +53,6 @@ export default function Home() {
         </Layout>
     );
 }
+
+let games_link = ["casino", "roulette", "slots", "trade"];
+let games = ["Казино", "Рулетка", "Слоты", "Трейд"];
